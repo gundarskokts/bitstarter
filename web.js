c8d5bcd2965cde70d37212904b38c4be2867a289
Gundars Kokts <gundars.kokts@sbcglobal.net>
2013-07-06 17:15:35 +0000

    Initial non-empty buffer

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require ("fs");
-var buffer = new Buffer(256); 
+var buffer = new Buffer("I am a string buffer", "utf-8"); 
 
 app.get('/', function(request, response) {
   buffer = fs.readFileSync('./index.html');
